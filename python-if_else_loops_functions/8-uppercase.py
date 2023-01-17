85c5939933dcdb08b185bdc035d7b2dbf184e547
simonrichard-dev <simon.insa@gmail.com>
2023-01-17 18:46:46 +0100

    add 8-uppercase.py file

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for alpha in str:
+        alpha = ord(alpha)
+        if alpha in range (97, 123):
+            alpha = alpha - 32
+        print("{:c}".format(alpha), end = "")
+    print("")
