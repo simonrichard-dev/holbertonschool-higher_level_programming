fe13465611d1233388b333284216341e5475be49
simonrichard-dev <simon.insa@gmail.com>
2023-01-18 11:30:49 +0100

    fix style error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         alpha = ord(alpha)
         if alpha in range(97, 123):
             alpha = alpha - 32
-        print("{:c}".format(alpha), end = "")
+        print("{:c}".format(alpha), end="")
     print("")
