408fead26e35d4d7ab7d92041592ac370369f389
simonrichard-dev <simon.insa@gmail.com>
2023-01-23 12:13:57 +0100

    fix error output for all output 1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return(None)
-    elif idx > range(len(my_list)):
+    elif idx not in range(len(my_list)):
         return(None)
     else:
         return my_list[idx]
