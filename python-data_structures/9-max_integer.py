25fb10be5f0201c3d586b26d61fb60eafbf6bc40
simonrichard-dev <simon.insa@gmail.com>
2023-01-24 14:12:54 +0100

    add 9-max_integer.py file

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return(None)
+    length = len(my_list)
+    maxi = my_list[0]
+    for i in range (0, length):
+        if my_list[i] > maxi:
+            maxi = my_list[i]
+        i += 1
+    return(maxi)
