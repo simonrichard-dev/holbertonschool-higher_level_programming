ee050dc99770ee04b86bef4be87e2af5723907ac
simonrichard-dev <simon.insa@gmail.com>
2023-01-24 13:19:40 +0100

    add 8-multiple_returns.py file

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        length = len(sentence)
+        first = None
+    else:
+        length = len(sentence)
+        first = sentence[0]
+    return((length, first))
