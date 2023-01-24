7c73d3ab1cec32d8d27d38adeeb6929e4db04200
simonrichard-dev <simon.insa@gmail.com>
2023-01-24 11:13:33 +0100

    add 7-add_tuple.py file

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    res=[]
+    for i in range(0,2):
+        if len(tuple_a) < 2 :
+            tuple_a += (0, 0)
+        elif len(tuple_b) < 2 :
+            tuple_b += (0, 0)
+        res.append(tuple_a[i]+tuple_b[i])
+        i += 1
+    res=tuple(res)
+    return(res)
