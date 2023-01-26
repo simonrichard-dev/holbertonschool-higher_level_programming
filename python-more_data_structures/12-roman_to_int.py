750f1f07efe7295d118dee7c95fdcac45813053d
simonrichard-dev <simon.insa@gmail.com>
2023-01-26 10:53:21 +0100

    fix PEP8 style missing spaces line 6 - 12-roman_to_int.py file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     if type(roman_string) != str:
         return(0)
     integer = 0
-    roman = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
         if i != (len(roman_string) - 1) and roman[roman_string[i]] \
                 < roman[roman_string[i + 1]]:
