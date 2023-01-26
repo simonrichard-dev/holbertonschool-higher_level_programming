cbef504a05f9227b5cd661409e6e84b6bc604554
simonrichard-dev <simon.insa@gmail.com>
2023-01-26 17:12:03 +0100

    add 0-safe_print_list.py file

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end='')
+            j += 1
+        print()
+        return(j)
+    except:
+        print()
+        return(j)
