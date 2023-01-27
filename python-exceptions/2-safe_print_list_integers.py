d3874a0a49fc269651e1dd60d3704612a0b6f2e6
simonrichard-dev <simon.insa@gmail.com>
2023-01-27 11:01:30 +0100

    add 2-safe_print_list_integers.py file

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    #try:
+    for x in range(x):
+        try:
+            print("{:d}".format(my_list[x]), end="")
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            j += 1
+    print()
+    return(j)
