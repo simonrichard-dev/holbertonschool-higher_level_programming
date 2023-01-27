4fee8062912985155cbea502fb97b8b44e4bee18
simonrichard-dev <simon.insa@gmail.com>
2023-01-27 12:54:11 +0100

    fix PEP8 validation trailing whitespace - 3-safe_print_division.py file

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         r = a / b
     except ZeroDivisionError:
-        r = None    
+        r = None
     finally:
         print("Inside result: {}".format(r))
         return r
