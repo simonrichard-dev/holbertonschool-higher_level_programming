29f57b67c9d5e560a880d8f92b576996fc483ea3
simonrichard-dev <simon.insa@gmail.com>
2023-01-27 12:07:41 +0100

    add 3-safe_print_division.py file

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        r = a / b
+    except ZeroDivisionError:
+        r = None    
+    finally:
+        print("Inside result: {}".format(r))
+        return r
