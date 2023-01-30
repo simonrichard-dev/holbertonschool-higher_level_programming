d887e2f07f729e821883a14024d52dc9a50195f5
simonrichard-dev <simon.insa@gmail.com>
2023-01-30 15:16:46 +0100

    add 3-square.py file

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Area of a square - Python """
+
+
+class Square:
+    """ Area of a square - Python """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    
+    def area(self):
+        return(self.__size * self.__size)
