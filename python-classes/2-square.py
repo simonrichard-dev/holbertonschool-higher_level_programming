5c2c550740a1ec4c74686db87f738851ce3b5363
simonrichard-dev <simon.insa@gmail.com>
2023-01-30 15:03:58 +0100

    add 2-square.py file

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Size validation - Python """
+
+
+class Square:
+    """ Size validation - Python """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
