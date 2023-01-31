750fb19162cd4dabbc90e8698d4dc8d43e9be434
simonrichard-dev <simon.insa@gmail.com>
2023-01-31 20:12:24 +0100

    add file that add integer and test directory with file corresponding

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" addition function """
+
+def add_integer(a, b=98):
+    """ addition function """
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b =  int(b)
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+    return(a + b)
