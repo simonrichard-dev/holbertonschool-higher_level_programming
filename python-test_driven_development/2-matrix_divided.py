c68691cd7b09662b29f4f3eeebaa190916561da7
simonrichard-dev <simon.insa@gmail.com>
2023-02-01 18:02:22 +0100

    Fixing output and test and PeppaStyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -7,24 +7,26 @@ def matrix_divided(matrix, div):
     each row of the matrix must be of the same size
     div must be a number (integer or float)
     div can’t be equal to 0 """
-    
+
     for row in matrix:
         for data in row:
             if type(data) is not int and type(data) is not float:
                 raise TypeError("matrix must be a matrix "
                                 "(list of lists) of "
                                 "integers/floats")
+
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
                 raise TypeError("Each row of the matrix "
                                 "must have the same size")
-    
+
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     if div == 0:
-        raise ZeroDivisionError('division by zero')
+        raise ZeroDivisionError("division by zero")
 
-    new_matrix = list(map(lambda row: list(map(lambda x: round(x / div, 2), row)), matrix))
+    new_matrix = list(map(lambda row: list(map\
+            (lambda x: round(x / div, 2), row)), matrix))
     return new_matrix
