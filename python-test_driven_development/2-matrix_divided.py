d79d32eadfa23849eeddca1b9e28d73b74a9838c
simonrichard-dev <simon.insa@gmail.com>
2023-02-01 18:07:03 +0100

    fix PEP8 validation

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,6 +27,6 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    new_matrix = list(map(lambda row: list(map\
-            (lambda x: round(x / div, 2), row)), matrix))
+    new_matrix = list(map(lambda row: list(map(lambda x:\
+            round(x / div, 2), row)), matrix))
     return new_matrix
