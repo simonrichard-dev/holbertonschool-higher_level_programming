1f506a7d35e1f9c31d9a1e6d7081b68e5fb73c1c
simonrichard-dev <simon.insa@gmail.com>
2023-02-02 11:30:52 +0100

    fixing last Test and PEP8 files for task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
-
 """ Write a function that prints My name is <first name> <last name> """
+
+
 def say_my_name(first_name, last_name=""):
     """ first_name must be strings otherwise, raise a TypeError exception with\
         the message first_name must be a string.
@@ -12,7 +12,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError('first_name must be a string')
     if type(last_name) is not str:
         raise TypeError('last_name must be a string')
-    if first_name == None and last_name == None:
+    if first_name is None and last_name is None:
         raise TypeError("say_my_name() missing 1 required\
                 positional argument: 'first_name'")
     print("My name is {} {}".format(first_name, last_name))
