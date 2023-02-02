11bb8f995ba07abf5af6c80a2a7b6369d55b0149
simonrichard-dev <simon.insa@gmail.com>
2023-02-02 14:33:00 +0100

    add task files to print a square and tests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" function that prints a square with the character # """
+
+
+def print_square(size):
+    """ size is the size length of the square.
+        size must be an integer, otherwise raise a TypeError\
+        exception with the message size must be an integer.
+        if size is less than 0, raise a ValueError exception\
+        with the message size must be >= 0.
+        if size is a float and is less than 0, raise a TypeError\
+        exception with the message size must be an integer. """
+
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
