949aaa953bae7178f3913df7b9abaf5799ebcf56
simonrichard-dev <simon.insa@gmail.com>
2023-02-02 14:54:13 +0100

    fix output without argument

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -15,6 +15,8 @@ def print_square(size):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
+    if size == None:
+        raise TypeError("TypeError: print_square() missing 1 required positional argument: 'size'")
 
     for i in range(size):
         for j in range(size):
