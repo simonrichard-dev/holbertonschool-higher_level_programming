a34ee7656e5a4cb4f75681a5554022a74f30377f
simonrichard-dev <simon.insa@gmail.com>
2023-02-03 14:19:41 +0100

    add 1-rectangle.py file

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+    This module contain an empty Rectangle class with
+    width and height attributes and also a proprety and
+    proprety setter
+"""
+
+class Rectangle:
+        """ An rectangle class with width and height properties """
+
+
+    def __init__(self, width=0, heigth=0):
+        """ Initialize a new instance """
+
+
+        self.__width = width
+
+        @property
+        def width(self):
+            """ get the width proprety """
+
+            return self.__width
+
+        @width.setter
+        def width(self, value):
+            """ Set the width proprety """
+
+            if type(value) is not int:
+                raise TypeError("width must be an integer")
+            if width < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+        self.__heigth = heigth
+
+        @property
+        def heigth(self):
+            return self.__height
+
+        @heigth.setter
+        def heigth(self, value):
+
+            if type(value) is not int:
+                raise TypeError("heigth must be an integer")
+            if size < 0:
+                raise ValueError("heigth must be >= 0")
+            else:
+                self.__heigth = heigth
