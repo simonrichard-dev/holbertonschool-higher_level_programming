776c6fc615feb2884589b38c480fea1e487fd6af
simonrichard-dev <simon.insa@gmail.com>
2023-02-03 14:51:56 +0100

    fixing elif conditions 1-rectangle.py file

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
 
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -44,7 +44,7 @@ class Rectangle:
 
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
