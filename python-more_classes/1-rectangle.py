a920b7aa66e3c14475cfe1e67292e62be66d4e31
simonrichard-dev <simon.insa@gmail.com>
2023-02-03 14:53:55 +0100

    fixing xconditions errors 1-rectangle.py file

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,10 +27,10 @@ class Rectangle:
 
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        
+        self.__width = value
 
     @property
     def height(self):
@@ -44,7 +44,7 @@ class Rectangle:
 
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        
+        self.__height = value
