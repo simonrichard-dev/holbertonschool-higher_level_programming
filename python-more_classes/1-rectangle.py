b8f0bd333cf024ed8d824751c0c07d4715aae678
simonrichard-dev <simon.insa@gmail.com>
2023-02-03 14:49:53 +0100

    fixing value definition in if conditions 1-rectangle.py file

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
 
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -44,7 +44,7 @@ class Rectangle:
 
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
