3e70a2f0ebc244515e1ce03e4439beae7d3a15d7
simonrichard-dev <simon.insa@gmail.com>
2023-02-07 16:28:36 +0100

    add files for task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class MyList that inherits from list """
+
+
+class MyList(list):
+    """ class Mylist that inherits from list """
+    
+    def __init__(self):
+        super().__init__()
+
+    def print_sorted(self):
+        print(sorted(self))
