09fc283d3fd0defd233bde1c40f685e816082eec
simonrichard-dev <simon.insa@gmail.com>
2023-02-07 13:03:29 +0100

    fix PEP8 validation - 0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" 
+"""
     checks the list of available attributes and methods
     of an object
 """
 
 
 def lookup(obj):
-    """ 
+    """
         function that returns the list of available attributes and methods
         of an object
     """
 
-    return(dir(obj))
+    return dir(obj)
