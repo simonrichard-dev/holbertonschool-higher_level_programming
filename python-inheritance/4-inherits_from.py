8e9a3d88a0a17992d78b9298d972d8b479e1e8a5
simonrichard-dev <simon.insa@gmail.com>
2023-02-08 16:28:49 +0100

    fix syntax error and PEP8 validation 4-inherits_from.py file

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,11 +4,12 @@
 
 def inherits_from(obj, a_class):
     """
-    Function that returns True if the object is an instance of a class that inherited
-    (directly or indirectly) from the specified class; otherwise False
+    Function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class;
+    otherwise False
     Args:
         obj: an object
-        a_class: class type    
+        a_class: class type
     """
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
