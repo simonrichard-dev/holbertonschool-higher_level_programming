f3ee8a4cb7587e2ca947537f986c69b3a1145387
simonrichard-dev <simon.insa@gmail.com>
2023-02-10 14:29:44 +0100

    add 8-class_to_json.py file

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""  Class to JSON """
+
+
+def class_to_json(obj):
+    """ function that returns the dictionary
+     description with simple data structure  """
+    return obj.__dict__
