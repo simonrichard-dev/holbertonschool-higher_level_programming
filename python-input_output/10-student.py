a89396a9033a5f331e7ebdd5bb784498464a4123
simonrichard-dev <simon.insa@gmail.com>
2023-02-10 16:33:26 +0100

    fix PEP8 validation - 10-student.py file

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,7 +11,7 @@ class Student ():
 
     def __init__(self, first_name, last_name, age):
         self.age = age
-        self.last_name = last_name       
+        self.last_name = last_name
         self.first_name = first_name
 
     def to_json(self, attrs=None):
