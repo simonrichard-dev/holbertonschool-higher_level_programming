5c9f699cfb68a4fc8a59573dc0b8d1633d185843
simonrichard-dev <simon.insa@gmail.com>
2023-02-10 15:23:50 +0100

    add 9-student.py file

---
+++ python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Student to JSON """
+import json
+
+
+class Student ():
+    """ class Student that defines a student by:
+        Public instance attributes:
+         - first_name
+         - last_name
+         - age """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+
+        return self.__dict__
