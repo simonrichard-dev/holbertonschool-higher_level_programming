95fc3b090906cc098a57bb81a58a28c17e8d5194
simonrichard-dev <simon.insa@gmail.com>
2023-02-15 11:26:28 +0100

    update square.py for task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -18,3 +18,15 @@ class Square(Rectangle):
         y = self.y
 
         return f"[Square] ({identifier}) {x}/{y} - {size}"
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
+        self.__size = value
