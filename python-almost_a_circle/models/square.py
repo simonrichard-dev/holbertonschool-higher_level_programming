d5216aa43a0cb3f81335baa551c5e1f001054f7b
simonrichard-dev <simon.insa@gmail.com>
2023-02-15 11:02:02 +0100

    add square.py file for task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" First Square """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ inherits from Rectangle """
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ Returns the [Rectangle] [<id>] - <size> """
+
+        identifier = self.id
+        size = self.width
+        x = self.x
+        y = self.y
+
+        return f"[Square] ({identifier}) {x}/{y} - {size}"
