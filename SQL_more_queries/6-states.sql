06e6a9419ac3e6850083d28bd86c61be2446195d
simonrichard-dev <simon.insa@gmail.com>
2023-03-15 18:19:26 +0100

    add 6-states.sql file

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT UNIQUE AUTO_INCREMENT,
+    name VARCHAR(256)NOT NULL,
+    PRIMARY KEY (id)
+);
