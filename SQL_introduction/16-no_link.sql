f10a4d22ee637a0d3f5c9e561f5c0a0afb5a7792
simonrichard-dev <simon.insa@gmail.com>
2023-03-15 09:03:21 +0100

    add 16-no_link.sql file

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,13 @@
+-- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT
+    score,
+    name
+FROM second_table
+    WHERE 
+    (
+        score IS NOT NULL
+    AND
+        id IS NOT NULL
+    AND
+        name IS NOT NULL
+    );
