49f97e851dd389389a2f17d4f995bce95340b077
simonrichard-dev <simon.insa@gmail.com>
2023-03-16 12:54:58 +0100

    add 10-genre_id_by_show.sql file

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- genre id by show
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM
+    tv_shows,
+    tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
