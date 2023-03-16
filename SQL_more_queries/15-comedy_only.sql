69394eb4f290a83a56f3530a4fb50ef15526af18
simonrichard-dev <simon.insa@gmail.com>
2023-03-16 17:17:11 +0100

    add 15-comedy_only.sql & 16-shows_by_genre.sql files

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- comedy only
+SELECT tv_shows.title 'title'
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY title;
