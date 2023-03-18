72892c34a0b122fc2d1377e404edf891da4a9d46
simonrichard-dev <simon.insa@gmail.com>
2023-03-18 14:07:20 +0100

    add 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-""" lists all states in input from the database hbtn_0e_0_usa"""
-
+"""
+    lists all states with the name in input
+    from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    connection = MySQLdb.connect(user=username, passwd=password,
-                                 db=database, host="localhost",
-                                 port=3306)
-
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        database=sys.argv[3]
+        )
+    name_searched = sys.argv[4]
     cursor = connection.cursor()
-    request = "SELECT * FROM states BINARY'{}'\
-        ORDER BY states.id"
-    cursor.execute(request)
-
-    statelist = cursor.fetchall()
-
-    for state in statelist:
-        print(state)
-
+    cursor.execute("SELECT * \
+                FROM states \
+                WHERE BINARY name = '{}' \
+                ORDER BY states.id ASC".format(name_searched))
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
     cursor.close()
     connection.close()
