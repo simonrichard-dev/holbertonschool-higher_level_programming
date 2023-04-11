f1c80508f45b06d93530f65632d531bf40c515d1
simonrichard-dev <simon.insa@gmail.com>
2023-04-11 16:11:39 +0200

    fix number of console.log (only 2)

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -7,8 +7,6 @@ if (Number.isInteger(number)) {
   for (let i = 0; i < number; i++) {
     console.log(array[0]);
   }
-} else if (number < 0) {
-  console.log('');
 } else {
   console.log('Missing number of occurrences');
 }
