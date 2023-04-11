2c7e220125adedbd775ea7c9cf11a32f4ff560c7
simonrichard-dev <simon.insa@gmail.com>
2023-04-11 09:40:17 +0200

    add 1-multi_languages.js file

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myVar1 = 'C is fun';
+console.log(myVar1);
+
+const myVar2 = 'Python is cool';
+console.log(myVar2);
+
+const myVar3 = 'JavaScript is amazing';
+console.log(myVar3);
