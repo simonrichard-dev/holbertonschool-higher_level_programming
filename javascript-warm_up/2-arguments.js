13f872467053f5e0bb5cf698b37e37c9cde57c82
simonrichard-dev <simon.insa@gmail.com>
2023-04-11 11:56:57 +0200

    fix semistandard indentation and singlequote in lines 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const { argv } = require("process");
+const { argv } = require('process');
 
-const Arguments = argv.length
+const Arguments = argv.length;
 
 if (Arguments === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (Arguments === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
