eab88a045d6fb366bba1152d16108b011cb6dfdb
simonrichard-dev <simon.insa@gmail.com>
2023-04-11 16:47:07 +0200

    restore 7-multi_c.js file

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('process');
 const number = parseInt(argv[2]);
+const array = ['C is fun'];
 
 if (Number.isInteger(number)) {
   for (let i = 0; i < number; i++) {
-    console.log('X'.repeat(number));
-  } 
+    console.log(array[0]);
+  }
+} else if (number < 0) {
+  console.log('');
 } else {
-  console.log('Missing size');
+  console.log('Missing number of occurrences');
 }
