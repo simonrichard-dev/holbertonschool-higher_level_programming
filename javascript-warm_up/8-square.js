9e877132a9842d0f7c3920b44c295107da30e700
simonrichard-dev <simon.insa@gmail.com>
2023-04-11 16:39:37 +0200

    add 8-square.js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const number = parseInt(argv[2]);
+
+if (Number.isInteger(number)) {
+  for (let i = 0; i < number; i++) {
+    console.log('X'.repeat(number));
+  } 
+} else {
+  console.log('Missing size');
+}
