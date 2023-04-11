7d8f5f45376e22350a3816e52324958fb550362f
simonrichard-dev <simon.insa@gmail.com>
2023-04-11 11:52:28 +0200

    add 2-arguments.js file

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { argv } = require("process");
+
+const Arguments = argv.length
+
+if (Arguments === 2) {
+    console.log('No argument');
+} else if (Arguments === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
