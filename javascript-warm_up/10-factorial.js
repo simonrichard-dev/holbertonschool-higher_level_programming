b8b382bb3daf95f65bce932429160bfee1d56bf5
simonrichard-dev <simon.insa@gmail.com>
2023-04-12 22:39:25 +0200

    fix input NaN case

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,19 +3,19 @@ const { argv } = require('process');
 const number = parseInt(argv[2]);
 
 const answer = factorial(number);
-
-function factorial (n) {
-    /* if(n < 0){
-        return "number has to be positive."
-    } */
-    
-    //base case
-    if(n == 0 || n == 1){
-        return 1;
-    //recursive case
-    }else{
-        return n * factorial(n-1);
+console.log(number)
+function factorial (n) 
+{
+  if (Number.isInteger(number)) {
+    if (n === 0 || n === 1) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
     }
+  } else {
+    const number = 1
+    return number;
+  }
 }
 
 console.log(answer);
