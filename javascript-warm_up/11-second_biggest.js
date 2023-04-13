36c77bfa6e2b4fdb9e6d87f33c1df8054ec3fdc0
simonrichard-dev <simon.insa@gmail.com>
2023-04-13 09:57:49 +0200

    add 11-second_biggest.js file

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const array = [];
+
+if (argv[2] === undefined) {
+  console.log(0);
+} else {
+  let count = 0;
+  for (let i = 2; argv[i]; i++) {
+    const number = parseInt(argv[i]);
+    array.push(number);
+    count += 1;
+  } if (count === 1) {
+    console.log(0);
+  } else {
+    array.sort();
+    console.log(array[count - 2]);
+  }
+}
