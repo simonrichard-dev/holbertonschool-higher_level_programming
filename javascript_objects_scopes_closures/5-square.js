688c9cd3a61049ef07ca4f87905198a9ae53a673
simonrichard-dev <simon.insa@gmail.com>
2023-04-14 15:06:51 +0200

    change line 35 by 36

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,40 +1,40 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let i = 0; i < this.height; i++) {
-        let line = '';
-        for (let j = 0; j < this.width; j++) {
-          line += 'X';
-        }
-        console.log(line);
-      }
-    }
-  
-    double () {
-      this.height = this.height * 2;
-      this.width = this.width * 2;
-    }
-  
-    rotate () {
-      const h = this.height;
-      this.height = this.width;
-      this.width = h;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 
-class Square extends Rectangle {
-    constructor(size) {
-        super(size);
-        this.height = size;
-        this.width = size;
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
     }
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+
+  rotate () {
+    const h = this.height;
+    this.height = this.width;
+    this.width = h;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size);
+    this.width = size;
+    this.height = size;
+  }
 }
 
 module.exports = Rectangle;
