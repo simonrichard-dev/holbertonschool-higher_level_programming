579375dfbcbed1dae715f0060912ff3b32f2ebae
simonrichard-dev <simon.insa@gmail.com>
2023-04-14 14:58:27 +0200

    add 5-square.js file

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+  
+    print () {
+      for (let i = 0; i < this.height; i++) {
+        let line = '';
+        for (let j = 0; j < this.width; j++) {
+          line += 'X';
+        }
+        console.log(line);
+      }
+    }
+  
+    double () {
+      this.height = this.height * 2;
+      this.width = this.width * 2;
+    }
+  
+    rotate () {
+      const h = this.height;
+      this.height = this.width;
+      this.width = h;
+    }
+  }
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size);
+        this.height = size;
+        this.width = size;
+    }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
