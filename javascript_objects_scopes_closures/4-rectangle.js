f0f92ba9337e245bb6ce955e685beb8d48dcb9ff
simonrichard-dev <simon.insa@gmail.com>
2023-04-14 13:22:03 +0200

    add 4-rectangle.js

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+
+  rotate () {
+    const h = this.height;
+    this.height = this.width;
+    this.width = h;
+  }
+}
+
+module.exports = Rectangle;
