0e35df1c36687c266f0bfdc602abee754a0c9c15
simonrichard-dev <simon.insa@gmail.com>
2023-04-14 16:18:07 +0200

    optimize with import 4-rectangle.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,35 +1,7 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Rect = require('./4-rectangle')
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let line = '';
-      for (let j = 0; j < this.width; j++) {
-        line += 'X';
-      }
-      console.log(line);
-    }
-  }
-
-  double () {
-    this.height = this.height * 2;
-    this.width = this.width * 2;
-  }
-
-  rotate () {
-    const h = this.height;
-    this.height = this.width;
-    this.width = h;
-  }
-}
-
-class Square extends Rectangle {
+class Square extends Rect {
   constructor (size) {
     super(size);
     if (size <= 0) {
@@ -42,5 +14,4 @@ class Square extends Rectangle {
   }
 }
 
-module.exports = Rectangle;
 module.exports = Square;
