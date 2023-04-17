8f5825952105aea988ed33958b22e6f20bade77c
simonrichard-dev <simon.insa@gmail.com>
2023-04-17 14:38:22 +0200

    add response in variable

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/',
-  function (error, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  function (error, response, body) {
     if (!error) {
       const Film = JSON.parse(body);
       console.log(Film.title);
