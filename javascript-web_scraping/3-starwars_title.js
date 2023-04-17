f1f7d103008ed9d1b2a83ec402d929f9f2d1763e
simonrichard-dev <simon.insa@gmail.com>
2023-04-17 14:35:03 +0200

    add a / in the URL

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/',
   function (error, body) {
     if (!error) {
       const Film = JSON.parse(body);
