91b8c31981d3794981bb096e26b86f72047dc1f0
simonrichard-dev <simon.insa@gmail.com>
2023-04-17 14:29:29 +0200

    add 3-starwars_title.js file

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  function (error, body) {
+    if (!error) {
+      const Film = JSON.parse(body);
+      console.log(Film.title);
+    }
+  });
